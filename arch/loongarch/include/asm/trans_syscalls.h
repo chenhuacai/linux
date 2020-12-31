6df28f6968b1cfe2f4f87d394ed98b2e116cfe16
Huacai Chen <chenhuacai@loongson.cn>
2020-12-31 15:13:33 +0800

    LoongArch: Add Loongson Binary Translation syscall support
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

---
+++ arch/loongarch/include/asm/trans_syscalls.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 Loongson Technology Corporation Limited
+ * Authors: Hanlu Li <lihanlu@loongson.cn>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#ifndef _TRANS_SYSCALLS_H_
+#define _TRANS_SYSCALLS_H_
+#include <asm/trans_mips_syscalls.h>
+#endif
