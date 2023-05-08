f63b87c98cc77b4ec489e17fa3e754b26baec13e
Huacai Chen <chenhuacai@loongson.cn>
2023-05-08 20:48:44 +0800

    kthread: Unify kernel_thread() and user_mode_thread()
    Commit 343f4c49f2438d8 ("kthread: Don't allocate kthread_struct for init
    and umh") introduces a new function user_mode_thread() for init and umh.
    
    init and umh are different from typical kernel threads since the don't
    need a "kthread" struct and they will finally become user processes by
    calling kernel_execve(), but on the other hand, they are also different
    from typical user mode threads (they have no "mm" structs at creation
    time, which is traditionally used to distinguish a user thread and a
    kernel thread).
    
    So I think it is reasonable to treat init and umh as "special kernel
    threads". Then let's unify the kernel_thread() and user_mode_thread()
    to kernel_thread() again, and add a new 'user' parameter for init and
    umh.
    
    This also makes code simpler.
    
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2698,7 +2698,7 @@ pid_t kernel_clone(struct kernel_clone_args *args)
  * Create a kernel thread.
  */
 pid_t kernel_thread(int (*fn)(void *), void *arg, const char *name,
-		    unsigned long flags)
+		    unsigned long flags, bool user)
 {
 	struct kernel_clone_args args = {
 		.flags		= ((flags | CLONE_VM | CLONE_UNTRACED) & ~CSIGNAL),
@@ -2706,22 +2706,7 @@ pid_t kernel_thread(int (*fn)(void *), void *arg, const char *name,
 		.fn		= fn,
 		.fn_arg		= arg,
 		.name		= name,
-		.kthread	= 1,
-	};
-
-	return kernel_clone(&args);
-}
-
-/*
- * Create a user mode thread.
- */
-pid_t user_mode_thread(int (*fn)(void *), void *arg, unsigned long flags)
-{
-	struct kernel_clone_args args = {
-		.flags		= ((flags | CLONE_VM | CLONE_UNTRACED) & ~CSIGNAL),
-		.exit_signal	= (flags & CSIGNAL),
-		.fn		= fn,
-		.fn_arg		= arg,
+		.kthread	= !user,
 	};
 
 	return kernel_clone(&args);
