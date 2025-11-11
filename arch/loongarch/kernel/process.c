8e546c2f23c1702709dd0447fbdb667fa4ef42fd
Huacai Chen <chenhuacai@loongson.cn>
2025-11-11 16:00:36 +0800

    LoongArch: Adjust process management for 32BIT/64BIT
    Adjust memory management for both 32BIT and 64BIT, including: CPU
    context switching, FPU loading/restoring, process dumping and process
    tracing routines.
    
    Q: Why modify switch.S?
    A: LoongArch32 has no ldptr.d/stptr.d instructions, and asm offsets of
       thead_struct members are too large to be filled in the 12b immediate
       field of ld.w/st.w.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/kernel/process.c
+++ arch/loongarch/kernel/process.c
@@ -377,8 +377,11 @@ void arch_trigger_cpumask_backtrace(const cpumask_t *mask, int exclude_cpu)
 	nmi_trigger_cpumask_backtrace(mask, exclude_cpu, raise_backtrace);
 }
 
-#ifdef CONFIG_64BIT
+#ifdef CONFIG_32BIT
+void loongarch_dump_regs32(u32 *uregs, const struct pt_regs *regs)
+#else
 void loongarch_dump_regs64(u64 *uregs, const struct pt_regs *regs)
+#endif
 {
 	unsigned int i;
 
@@ -395,4 +398,3 @@ void loongarch_dump_regs64(u64 *uregs, const struct pt_regs *regs)
 	uregs[LOONGARCH_EF_CSR_ECFG] = regs->csr_ecfg;
 	uregs[LOONGARCH_EF_CSR_ESTAT] = regs->csr_estat;
 }
-#endif /* CONFIG_64BIT */
