f7a8dd82559c25977ace4b2fd0c4500abd63596c
Bibo Mao <maobibo@loongson.cn>
2026-05-08 09:30:01 +0800

    LoongArch: KVM: Move some variable declarations to paravirt.h
    Some variables relative with paravirt feature are declared in the header
    file asm/qspinlock.h, however this file can be included only when option
    CONFIG_SMP is on. There is compiling warnings if CONFIG_SMP is off since
    variables are not declared.
    
    Move these variable declarations to header file asm/paravirt.h to avoid
    compiling warnings.
    
    Fixes: c43dce6f13fb ("LoongArch: KVM: Make vcpu_is_preempted() as a macro rather than function")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202605061313.O8Hswm2b-lkp@intel.com/
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/include/asm/paravirt.h
+++ arch/loongarch/include/asm/paravirt.h
@@ -2,7 +2,13 @@
 #ifndef _ASM_LOONGARCH_PARAVIRT_H
 #define _ASM_LOONGARCH_PARAVIRT_H
 
+#include <linux/jump_label.h>
+
 #ifdef CONFIG_PARAVIRT
+
+DECLARE_STATIC_KEY_FALSE(virt_preempt_key);
+DECLARE_STATIC_KEY_FALSE(virt_spin_lock_key);
+DECLARE_PER_CPU(struct kvm_steal_time, steal_time);
 
 int __init pv_ipi_init(void);
 int __init pv_time_init(void);
